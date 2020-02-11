f6ae1fe39d38c17ef7c15d36b26b4121f90eb799
Mirudistok <Mirudistok@yandex.ru>
2020-02-12 01:38:55 +0300

    Create section products

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-       
+    const mixer = mixitup('.products__inner-box');
     
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,8 @@ gulp.task('js', () => {
 
 gulp.task('script', () => {
     return gulp.src([
-        'node_modules/slick-carousel/slick/slick.min.js',
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/mixitup/dist/mixitup.js'
     ])
             .pipe(concat('libs.min.js'))
             .pipe(uglify())
