b1247fd2282035a9726535042ab2606a4a27c6a4
Mirudistok <Mirudistok@yandex.ru>
2020-02-15 22:56:26 +0300

    Create page new-items and add plugin ionRangeSlider

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,8 @@ gulp.task('style', () => {
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/rateyo/src/jquery.rateyo.css'
+        'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
     ])      
             .pipe(concat('libs.min.css'))
             .pipe(cssmin())
@@ -42,7 +43,8 @@ gulp.task('script', () => {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/mixitup/dist/mixitup.js',
-        'node_modules/rateyo/src/jquery.rateyo.js'
+        'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
     ])
             .pipe(concat('libs.min.js'))
             .pipe(uglify())
